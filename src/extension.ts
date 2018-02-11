48398c0c8d99ac850f88aff610072b7eb7ce5a24
sheche@microsoft.com <Szbn11dtsb`>
2018-02-11 21:48:59 +0800

    init project

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+"use strict";
+
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+
+}
+
+// tslint:disable-next-line:no-empty
+export function deactivate() { }
