f9c1b0975ba9fe9d6fcc36ed4015bd9fd1f98ef1
Sheng Chen <sheche@microsoft.com>
2018-03-26 18:22:31 +0800

    quotes the leetcode-cli path to treat the whitespaces

--- src/shared.ts
+++ src/shared.ts
@@ -3,7 +3,7 @@
 import * as path from "path";
 import * as vscode from "vscode";
 
-export const leetCodeBinaryPath: string = path.join(__dirname, "..", "..", "node_modules", "leetcode-cli", "bin", "leetcode");
+export const leetCodeBinaryPath: string = `"${path.join(__dirname, "..", "..", "node_modules", "leetcode-cli", "bin", "leetcode")}"`;
 
 export interface IQuickItemEx<T> extends vscode.QuickPickItem {
     value: T;
