9c01549c77495cf1f7eb36797addd43609039374
Vigilans <vigilans@foxmail.com>
2019-03-08 11:04:22 +0800

    Add markdown style support for SolutionProvider

--- src/leetCodeSolutionProvider.ts
+++ src/leetCodeSolutionProvider.ts
@@ -3,6 +3,8 @@
 
 import * as hljs from "highlight.js";
 import * as MarkdownIt from "markdown-it";
+import * as path from "path";
+import * as vscode from "vscode";
 import { Disposable, ExtensionContext, ViewColumn, WebviewPanel, window } from "vscode";
 import { Solution } from "./shared";
 
@@ -11,6 +13,7 @@ class LeetCodeSolutionProvider implements Disposable {
     private context: ExtensionContext;
     private panel: WebviewPanel | undefined;
     private markdown: MarkdownIt;
+    private markdownPath: string; // path of vscode built-in markdown extension
     private solution: Solution;
 
     public initialize(context: ExtensionContext): void {
@@ -20,6 +23,7 @@ class LeetCodeSolutionProvider implements Disposable {
             typographer: true,
             highlight: this.codeHighlighter.bind(this),
         });
+        this.markdownPath = path.join(process.env.VSCODE_CWD as string, "resources", "app", "extensions", "markdown-language-features");
     }
 
     public async show(solutionString: string): Promise<void> {
@@ -27,6 +31,7 @@ class LeetCodeSolutionProvider implements Disposable {
             this.panel = window.createWebviewPanel("leetCode", "Top voted solution", ViewColumn.Active, {
                 retainContextWhenHidden: true,
                 enableFindWidget: true,
+                localResourceRoots: [vscode.Uri.file(path.join(this.markdownPath, "media"))],
             });
 
             this.panel.onDidDispose(() => {
@@ -36,7 +41,7 @@ class LeetCodeSolutionProvider implements Disposable {
 
         this.solution = this.parseSolution(solutionString);
         this.panel.title = this.solution.title;
-        this.panel.webview.html = this.getWebViewContent(this.solution.body);
+        this.panel.webview.html = this.getWebViewContent(this.solution);
         this.panel.reveal(ViewColumn.Active);
     }
 
@@ -73,21 +78,27 @@ class LeetCodeSolutionProvider implements Disposable {
         return ""; // use external default escaping
     }
 
-    private getWebViewContent(body: string): string {
-        return this.markdown.render(body);
-        // return `
-        //     <!DOCTYPE html>
-        //     <html lang="en">
-        //     <head>
-        //         <meta charset="UTF-8">
-        //         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        //         <title>LeetCode Top Voted Solution</title>
-        //     </head>
-        //     <body>
-        //         <pre>${body}</pre>
-        //     </body>
-        //     </html>
-        // `;
+    private getMarkdownStyles(): vscode.Uri[] {
+        const stylePaths: string[] = require(path.join(this.markdownPath, "package.json"))["contributes"]["markdown.previewStyles"];
+        return stylePaths.map((p: string) => vscode.Uri.file(path.join(this.markdownPath, p)).with({ scheme: "vscode-resource" }));
+    }
+
+    private getWebViewContent(solution: Solution): string {
+        const styles: string = this.getMarkdownStyles()
+            .map((style: vscode.Uri) => `<link rel="stylesheet" type="text/css" href="${style.toString()}">`)
+            .join("\n");
+        const body: string = this.markdown.render(solution.body);
+        return `
+            <!DOCTYPE html>
+            <html>
+            <head>
+                ${styles}
+            </head>
+            <body class="vscode-body 'scrollBeyondLastLine' 'wordWrap' 'showEditorSelection'">
+                ${body}
+            </body>
+            </html>
+        `;
     }
 }
 
