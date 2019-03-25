83e1ac120c5aa863b56d9724562ada423bcf017d
Vigilans <vigilans@foxmail.com>
2019-03-25 21:30:27 +0800

    Append data result to error object generated on closing childProc

--- src/utils/cpUtils.ts
+++ src/utils/cpUtils.ts
@@ -5,6 +5,10 @@ import * as cp from "child_process";
 import * as vscode from "vscode";
 import { leetCodeChannel } from "../leetCodeChannel";
 
+interface IExecError extends Error {
+    result?: string;
+}
+
 export async function executeCommand(command: string, args: string[], options: cp.SpawnOptions = { shell: true }): Promise<string> {
     return new Promise((resolve: (res: string) => void, reject: (e: Error) => void): void => {
         let result: string = "";
@@ -20,9 +24,14 @@ export async function executeCommand(command: string, args: string[], options: c
         childProc.stderr.on("data", (data: string | Buffer) => leetCodeChannel.append(data.toString()));
 
         childProc.on("error", reject);
+
         childProc.on("close", (code: number) => {
             if (code !== 0 || result.indexOf("ERROR") > -1) {
-                reject(new Error(`Command "${command} ${args.toString()}" failed with exit code "${code}".`));
+                const error: IExecError = new Error(`Command "${command} ${args.toString()}" failed with exit code "${code}".`);
+                if (result) {
+                    error.result = result; // leetcode-cli may print useful content by exit with error code
+                }
+                reject(error);
             } else {
                 resolve(result);
             }
