e1f3206756c1909c36a3832bec8f010e4e18bf8e
Sheng Chen <sheche@microsoft.com>
2019-07-12 15:16:12 +0800

    feat: Can select folder to save the LeetCode files (#365)

--- src/utils/uiUtils.ts
+++ src/utils/uiUtils.ts
@@ -92,6 +92,18 @@ export async function showFileSelectDialog(): Promise<vscode.Uri[] | undefined> 
     return await vscode.window.showOpenDialog(options);
 }
 
+export async function showDirectorySelectDialog(): Promise<vscode.Uri[] | undefined> {
+    const defaultUri: vscode.Uri | undefined = vscode.workspace.rootPath ? vscode.Uri.file(vscode.workspace.rootPath) : undefined;
+    const options: vscode.OpenDialogOptions = {
+        defaultUri,
+        canSelectFiles: false,
+        canSelectFolders: true,
+        canSelectMany: false,
+        openLabel: "Select",
+    };
+    return await vscode.window.showOpenDialog(options);
+}
+
 export async function openUrl(url: string): Promise<void> {
     vscode.commands.executeCommand("vscode.open", vscode.Uri.parse(url));
 }
