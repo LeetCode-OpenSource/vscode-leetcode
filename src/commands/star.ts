395126da5b5a8497fbf29ddd850c875851ce5292
yihong0618 <zouzou0208@gmail.com>
2020-07-20 00:09:56 +0800

    add: star command in shortcuts

--- src/commands/star.ts
+++ src/commands/star.ts
@@ -2,6 +2,7 @@
 // Copyright (c) jdneo. All rights reserved.
 // Licensed under the MIT license.
 
+import { customCodeLensProvider } from "../codelens/CustomCodeLensProvider";
 import { LeetCodeNode } from "../explorer/LeetCodeNode";
 import { leetCodeTreeDataProvider } from "../explorer/LeetCodeTreeDataProvider";
 import { leetCodeExecutor } from "../leetCodeExecutor";
@@ -10,7 +11,8 @@ import { DialogType, promptForOpenOutputChannel } from "../utils/uiUtils";
 export async function addFavorite(node: LeetCodeNode): Promise<void> {
     try {
         await leetCodeExecutor.toggleFavorite(node, true);
-        leetCodeTreeDataProvider.refresh();
+        await leetCodeTreeDataProvider.refresh();
+        customCodeLensProvider.refresh();
     } catch (error) {
         await promptForOpenOutputChannel("Failed to add the problem to favorite. Please open the output channel for details.", DialogType.error);
     }
@@ -19,7 +21,8 @@ export async function addFavorite(node: LeetCodeNode): Promise<void> {
 export async function removeFavorite(node: LeetCodeNode): Promise<void> {
     try {
         await leetCodeExecutor.toggleFavorite(node, false);
-        leetCodeTreeDataProvider.refresh();
+        await leetCodeTreeDataProvider.refresh();
+        customCodeLensProvider.refresh();
     } catch (error) {
         await promptForOpenOutputChannel("Failed to remove the problem from favorite. Please open the output channel for details.", DialogType.error);
     }
