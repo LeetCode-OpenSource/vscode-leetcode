1d141cf601ae6b0a1089da3ab7eb317203c10417
Carlos Perez-Guerra <karlitoffel01@yahoo.com>
2021-10-26 09:01:16 +0200

    Fix prettier again

--- src/webview/leetCodePreviewProvider.ts
+++ src/webview/leetCodePreviewProvider.ts
@@ -8,54 +8,51 @@ import { ILeetCodeWebviewOption, LeetCodeWebview } from "./LeetCodeWebview";
 import { markdownEngine } from "./markdownEngine";
 
 class LeetCodePreviewProvider extends LeetCodeWebview {
-  protected readonly viewType: string = "leetcode.preview";
-  private node: IProblem;
-  private description: IDescription;
-  private sideMode: boolean = false;
 
-  public isSideMode(): boolean {
-    return this.sideMode;
-  }
+    protected readonly viewType: string = "leetcode.preview";
+    private node: IProblem;
+    private description: IDescription;
+    private sideMode: boolean = false;
 
-  public show(
-    descString: string,
-    node: IProblem,
-    isSideMode: boolean = false
-  ): void {
-    this.description = this.parseDescription(descString, node);
-    this.node = node;
-    this.sideMode = isSideMode;
-    this.showWebviewInternal();
-    // Comment out this operation since it sometimes may cause the webview become empty.
-    // Waiting for the progress of the VS Code side issue: https://github.com/microsoft/vscode/issues/3742
-    // if (this.sideMode) {
-    //     this.hideSideBar(); // For better view area
-    // }
-  }
-
-  protected getWebviewOption(): ILeetCodeWebviewOption {
-    if (!this.sideMode) {
-      return {
-        title: `${this.node.name}: Preview`,
-        viewColumn: ViewColumn.One,
-      };
-    } else {
-      return {
-        title: "Description",
-        viewColumn: ViewColumn.Two,
-        preserveFocus: true,
-      };
+    public isSideMode(): boolean {
+        return this.sideMode;
     }
-  }
 
-  protected getWebviewContent(): string {
-    const button: { element: string; script: string; style: string } = {
-      element: `<button id="solve">Code Now</button>`,
-      script: `const button = document.getElementById('solve');
+    public show(descString: string, node: IProblem, isSideMode: boolean = false): void {
+        this.description = this.parseDescription(descString, node);
+        this.node = node;
+        this.sideMode = isSideMode;
+        this.showWebviewInternal();
+        // Comment out this operation since it sometimes may cause the webview become empty.
+        // Waiting for the progress of the VS Code side issue: https://github.com/microsoft/vscode/issues/3742
+        // if (this.sideMode) {
+        //     this.hideSideBar(); // For better view area
+        // }
+    }
+
+    protected getWebviewOption(): ILeetCodeWebviewOption {
+        if (!this.sideMode) {
+            return {
+                title: `${this.node.name}: Preview`,
+                viewColumn: ViewColumn.One,
+            };
+        } else {
+            return {
+                title: "Description",
+                viewColumn: ViewColumn.Two,
+                preserveFocus: true,
+            };
+        }
+    }
+
+    protected getWebviewContent(): string {
+        const button: { element: string, script: string, style: string } = {
+            element: `<button id="solve">Code Now</button>`,
+            script: `const button = document.getElementById('solve');
                     button.onclick = () => vscode.postMessage({
                         command: 'ShowProblem',
                     });`,
-      style: `<style>
+            style: `<style>
                 #solve {
                     position: fixed;
                     bottom: 1rem;
@@ -73,41 +70,36 @@ class LeetCodePreviewProvider extends LeetCodeWebview {
                     border: 0;
                 }
                 </style>`,
-    };
-    const { title, url, category, difficulty, likes, dislikes, body } =
-      this.description;
-    const head: string = markdownEngine.render(`# [${title}](${url})`);
-    const info: string = markdownEngine.render(
-      [
-        `| Category | Difficulty | Likes | Dislikes |`,
-        `| :------: | :--------: | :---: | :------: |`,
-        `| ${category} | ${difficulty} | ${likes} | ${dislikes} |`,
-      ].join("\n")
-    );
-    const tags: string = [
-      `<details>`,
-      `<summary><strong>Tags</strong></summary>`,
-      markdownEngine.render(
-        this.description.tags
-          .map((t: string) => `[\`${t}\`](https://leetcode.com/tag/${t})`)
-          .join(" | ")
-      ),
-      `</details>`,
-    ].join("\n");
-    const companies: string = [
-      `<details>`,
-      `<summary><strong>Companies</strong></summary>`,
-      markdownEngine.render(
-        this.description.companies.map((c: string) => `\`${c}\``).join(" | ")
-      ),
-      `</details>`,
-    ].join("\n");
-    const links: string = markdownEngine.render(
-      `[Discussion](${this.getDiscussionLink(
-        url
-      )}) | [Solution](${this.getSolutionLink(url)})`
-    );
-    return `
+        };
+        const { title, url, category, difficulty, likes, dislikes, body } = this.description;
+        const head: string = markdownEngine.render(`# [${title}](${url})`);
+        const info: string = markdownEngine.render([
+            `| Category | Difficulty | Likes | Dislikes |`,
+            `| :------: | :--------: | :---: | :------: |`,
+            `| ${category} | ${difficulty} | ${likes} | ${dislikes} |`,
+        ].join("\n"));
+        const tags: string = [
+            `<details>`,
+            `<summary><strong>Tags</strong></summary>`,
+            markdownEngine.render(
+                this.description.tags
+                    .map((t: string) => `[\`${t}\`](https://leetcode.com/tag/${t})`)
+                    .join(" | "),
+            ),
+            `</details>`,
+        ].join("\n");
+        const companies: string = [
+            `<details>`,
+            `<summary><strong>Companies</strong></summary>`,
+            markdownEngine.render(
+                this.description.companies
+                    .map((c: string) => `\`${c}\``)
+                    .join(" | "),
+            ),
+            `</details>`,
+        ].join("\n");
+        const links: string = markdownEngine.render(`[Discussion](${this.getDiscussionLink(url)}) | [Solution](${this.getSolutionLink(url)})`);
+        return `
             <!DOCTYPE html>
             <html>
             <head>
@@ -134,94 +126,90 @@ class LeetCodePreviewProvider extends LeetCodeWebview {
             </body>
             </html>
         `;
-  }
+    }
 
-  protected onDidDisposeWebview(): void {
-    super.onDidDisposeWebview();
-    this.sideMode = false;
-  }
+    protected onDidDisposeWebview(): void {
+        super.onDidDisposeWebview();
+        this.sideMode = false;
+    }
 
-  protected async onDidReceiveMessage(message: IWebViewMessage): Promise<void> {
-    switch (message.command) {
-      case "ShowProblem": {
-        await commands.executeCommand("leetcode.showProblem", this.node);
-        break;
+    protected async onDidReceiveMessage(message: IWebViewMessage): Promise<void> {
+        switch (message.command) {
+            case "ShowProblem": {
+                await commands.executeCommand("leetcode.showProblem", this.node);
+                break;
+            }
+        }
+    }
+
+    // private async hideSideBar(): Promise<void> {
+    //     await commands.executeCommand("workbench.action.focusSideBar");
+    //     await commands.executeCommand("workbench.action.toggleSidebarVisibility");
+    // }
+
+    private parseDescription(descString, problem) {
+        // Parse body by looking for the first html tag
+        const bodyStartIdx = descString.search(/<.*>/);
+        const bodyRaw = descString.substring(bodyStartIdx);
+
+        const { name: title, tags, companies } = problem;
+        return {
+          title,
+          tags,
+          companies,
+          url: descString.match(/https:.*leetcode.*/)?.[0] || "??",
+          // Category is the first element in list
+          category: descString.match(/\*.*/)?.[0]?.slice(2) || "??",
+          // Difficulty is the first element in list with a percentage sign
+          difficulty: descString.match(/.*\%.*/)?.[0]?.slice(2) || "??",
+          likes:
+            descString
+              .match(/Likes.*?\n/)?.[0]
+              ?.split(": ")[1]
+              ?.trim() || "0",
+          dislikes:
+            descString
+              .match(/Dislikes.*?\n/)?.[0]
+              ?.split(": ")[1]
+              ?.trim() || "0",
+          body: bodyRaw.replace(
+            /<pre>[\r\n]*([^]+?)[\r\n]*<\/pre>/g,
+            "<pre><code>$1</code></pre>"
+          ),
+        };
       }
-    }
-  }
+    private getDiscussionLink(url: string): string {
+        const endPoint: string = getLeetCodeEndpoint();
+        if (endPoint === Endpoint.LeetCodeCN) {
+            return url.replace("/description/", "/comments/");
+        } else if (endPoint === Endpoint.LeetCode) {
+            return url.replace("/description/", "/discuss/?currentPage=1&orderBy=most_votes&query=");
+        }
 
-  // private async hideSideBar(): Promise<void> {
-  //     await commands.executeCommand("workbench.action.focusSideBar");
-  //     await commands.executeCommand("workbench.action.toggleSidebarVisibility");
-  // }
-
-  private parseDescription(descString, problem) {
-    // Parse body by looking for the first html tag
-    const bodyStartIdx = descString.search(/<.*>/);
-    const bodyRaw = descString.substring(bodyStartIdx);
-
-    const { name: title, tags, companies } = problem;
-    return {
-      title,
-      tags,
-      companies,
-      url: descString.match(/https:.*leetcode.*/)?.[0] || "??",
-      // Category is the first element in list
-      category: descString.match(/\*.*/)?.[0]?.slice(2) || "??",
-      // Difficulty is the first element in list with a percentage sign
-      difficulty: descString.match(/.*\%.*/)?.[0]?.slice(2) || "??",
-      likes:
-        descString
-          .match(/Likes.*?\n/)?.[0]
-          ?.split(": ")[1]
-          ?.trim() || "0",
-      dislikes:
-        descString
-          .match(/Dislikes.*?\n/)?.[0]
-          ?.split(": ")[1]
-          ?.trim() || "0",
-      body: bodyRaw.replace(
-        /<pre>[\r\n]*([^]+?)[\r\n]*<\/pre>/g,
-        "<pre><code>$1</code></pre>"
-      ),
-    };
-  }
-
-  private getDiscussionLink(url: string): string {
-    const endPoint: string = getLeetCodeEndpoint();
-    if (endPoint === Endpoint.LeetCodeCN) {
-      return url.replace("/description/", "/comments/");
-    } else if (endPoint === Endpoint.LeetCode) {
-      return url.replace(
-        "/description/",
-        "/discuss/?currentPage=1&orderBy=most_votes&query="
-      );
+        return "https://leetcode.com";
     }
 
-    return "https://leetcode.com";
-  }
-
-  private getSolutionLink(url: string): string {
-    return url.replace("/description/", "/solution/");
-  }
+    private getSolutionLink(url: string): string {
+        return url.replace("/description/", "/solution/");
+    }
 }
 
 interface IDescription {
-  title: string;
-  url: string;
-  tags: string[];
-  companies: string[];
-  category: string;
-  difficulty: string;
-  likes: string;
-  dislikes: string;
-  body: string;
+    title: string;
+    url: string;
+    tags: string[];
+    companies: string[];
+    category: string;
+    difficulty: string;
+    likes: string;
+    dislikes: string;
+    body: string;
 }
 
 interface IWebViewMessage {
-  command: string;
+    command: string;
 }
 
-export const leetCodePreviewProvider: LeetCodePreviewProvider =
-  new LeetCodePreviewProvider();
+export const leetCodePreviewProvider: LeetCodePreviewProvider = new LeetCodePreviewProvider();
+
 
