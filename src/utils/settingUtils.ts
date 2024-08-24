faf5b3dc3dfd76ddd0676a0edbc13e4a2bd8a77c
hortensia <824466875@qq.com>
2024-08-24 20:35:44 +0800

    add code preamble

--- src/utils/settingUtils.ts
+++ src/utils/settingUtils.ts
@@ -62,6 +62,130 @@ export function getDescriptionConfiguration(): IDescriptionConfiguration {
     return config;
 }
 
+const defaultPreambles = {
+    "cpp": `
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+using namespace std;
+    `
+}
+
+/**
+ * ## About
+ *
+ * Code preamble is a short code snippet adding to the beginning of the program. A typical one is:
+ *
+ *
+ * ```
+ * #include <iostream>
+ * using namespace std;
+ * ```
+ *
+ * This feature is useful. Maybe inserting header files is interesting for the first several times,
+ * it is dreadful if we need to add tons of it each time we do the problems, especially for a dreadful
+ * language like C/C++
+ *
+ * ## Configuration
+ *
+ * To add a preamble for one language, the user will follow this schema:
+ *
+ * ```
+ * "leetcode.language.preamble" = {
+ *      "cpp" : "#include <iostream>",
+ * }
+ * ```
+ *
+ * @param language
+ */
+export function getCodePreamble(language: string): string {
+    const preambles = getWorkspaceConfiguration().get<Record<string, string>>("language.preamble");
+    return preambles?.[language] ?? defaultPreambles?.[language] ?? "";
+}
+
 export interface IDescriptionConfiguration {
     showInComment: boolean;
     showInWebview: boolean;
